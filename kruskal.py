2d0bc48987e16291c8aa936c2fb3b3b04437810f
João Virgilio <jvirgiliomartins09@gmail.com>
2021-10-31 17:43:09 -0300

    alg-kruskal

--- kruskal.py
+++ kruskal.py
@@ -1,58 +1,59 @@
-class Graph:
-    def __init__(self, vertex):
-        self.V = vertex
-        self.graph = []
- 
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
- 
- 
-    def search(self, parent, i):
-        if parent[i] == i:
+import networkx as nx
+
+class Grafo:
+    def __init__(self, numero_vertice):
+        self.numero_vertice = numero_vertice
+        self.grafo = []
+
+    def add_aresta(sef, orig, dest, peso):
+        sef.grafo.append([orig, dest, peso])
+
+    def buscar(self, arv, i):
+        if arv[i] == i:
             return i
-        return self.search(parent, parent[i])
- 
-    def apply_union(self, parent, rank, x, y):
-        xroot = self.search(parent, x)
-        yroot = self.search(parent, y)
-        if rank[xroot] < rank[yroot]:
-            parent[xroot] = yroot
-        elif rank[xroot] > rank[yroot]:
-            parent[yroot] = xroot
+
+        return self.buscar(arv, arv[i])
+    
+    def uniao(self, arv, pai, orig, dest):
+        xorig = self.buscar(arv, orig)
+        ydest = self.buscar(arv, dest)
+        if pai[xorig] < pai[ydest]:
+            arv[xorig] = ydest
+        elif pai[xorig] > pai[ydest]:
+            arv[ydest] = xorig
         else:
-            parent[yroot] = xroot
-            rank[xroot] += 1
- 
-  
+            arv[ydest] = xorig
+            pai[xorig] += 1
+
     def kruskal(self):
         result = []
         i, e = 0, 0
-        self.graph = sorted(self.graph, key=lambda item: item[2])
-        parent = []
-        rank = []
-        for node in range(self.V):
-            parent.append(node)
-            rank.append(0)
-        while e < self.V - 1:
-            u, v, w = self.graph[i]
+        self.grafo = sorted(self.grafo, key=lambda item: item[2])
+        arv = []
+        pai = []
+        for node in range(self.numero_vertice):
+            arv.append(node)
+            pai.append(0)
+        while e < self.numero_vertice - 1:
+            orig, dest, peso = self.grafo[i]
             i = i + 1
-            x = self.search(parent, u)
-            y = self.search(parent, v)
+            x = self.buscar(arv, orig)
+            y = self.buscar(arv, dest)
             if x != y:
                 e = e + 1
-                result.append([u, v, w])
-                self.apply_union(parent, rank, x, y)
+                result.append([orig, dest, peso])
+                self.uniao(arv, pai, x, y)
+        print(result)
         for u, v, weight in result:
-            print("Edge:",u, v,end =" ")
+            print("Aresta:",u, v,end =" ")
             print("-",weight)
- 
- 
-g = Graph(5)
-g.add_edge(0, 1, 8)
-g.add_edge(0, 2, 5)
-g.add_edge(1, 2, 9)
-g.add_edge(1, 3, 11)
-g.add_edge(2, 3, 15)
-g.add_edge(2, 4, 10)
-g.add_edge(3, 4, 7)
-g.kruskal()
+
+grafo = Grafo(5)
+grafo.add_aresta(0, 1, 8)
+grafo.add_aresta(0, 2, 5)
+grafo.add_aresta(1, 2, 9)
+grafo.add_aresta(1, 3, 11)
+grafo.add_aresta(2, 3, 15)
+grafo.add_aresta(2, 4, 10)
+grafo.add_aresta(3, 4, 7)
+grafo.kruskal()
